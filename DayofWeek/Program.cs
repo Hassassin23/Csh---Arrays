6808e7c9af8d8f49be91db82c15203efd3e1d555
Paradox <93808441+FamilyPox@users.noreply.github.com>
2022-10-22 20:16:54 +0300

    Add files via upload

---
+++ DayofWeek/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DayofWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] weekDays = new string[] {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday" };
+            int day = int.Parse(Console.ReadLine());   
+            if(day < 1 || day > 7)
+            {
+                Console.WriteLine("Invalid day!");
+            }
+            else
+            {
+                Console.WriteLine(weekDays[day -1]);
+            }
+        }
+    }
+}
